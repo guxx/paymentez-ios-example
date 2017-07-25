9f90ea0aa91506f25cb16bcd36a4f7c6164bd29d
Gustavo Sotelo <guxsotelo@gmail.com>
2017-07-25 15:02:46 -0500

    first commit

---
+++ PaymentezSDKSwift/Libs-Bridging-Header.h
@@ -0,0 +1,7 @@
+//
+//  Libs-Bridging-Header.h
+//  PaymentezSDKSwift
+//
+//  Created by Gustavo Sotelo on 13/09/16.
+//  Copyright © 2016 Paymentez. All rights reserved.
+//
---
+++ PaymentezSDKSwift/libs/easy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo-iOS-Davivienda
+//
+//  Created by Jonathan Alonso Pinto on 8/8/16.
+//  Copyright © 2016 Jonathan Alonso Pinto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerEasy : UIViewController
+
+
+@end
+
